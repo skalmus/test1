78bda78cabc9bd542a0dec96afe4e01a586069a8
skalmus <sk@work.capital>
2015-06-14 00:27:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 3;
+        System.out.println(a);
+    }
+}
